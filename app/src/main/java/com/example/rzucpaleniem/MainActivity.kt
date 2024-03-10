8297146ff4cdccfe46f9c87f3659a6e164c23637
unknown <live66625@gmail.com>
2024-03-10 19:03:34 +0100

    pierwsza proba polaczenia z firebasem

--- app/src/main/java/com/example/rzucpaleniem/MainActivity.kt
+++ app/src/main/java/com/example/rzucpaleniem/MainActivity.kt
@@ -9,15 +9,21 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.rzucpaleniem.databinding.ActivityMainBinding
 
+import com.google.firebase.analytics.FirebaseAnalytics //firebase
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics //firebase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this) //firebase
 
         val navView: BottomNavigationView = binding.navView
 
@@ -32,5 +38,15 @@ class MainActivity : AppCompatActivity() {
 //         V Włączony actionBar u góry ekranu, aby wyłączyć odkomentować importa i linijke poniżej
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        // Log a custom event
+        val bundle = Bundle().apply {
+            putString(FirebaseAnalytics.Param.ITEM_NAME, "example_item")
+        }
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM, bundle)
+
+        // Set a user property
+        firebaseAnalytics.setUserProperty("favorite_screen", "stats_screen")
     }
 }
